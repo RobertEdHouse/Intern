76218bf77be4aa167285b3f2874fb18f1255e3f9
RobertEdHouse <robert.edwin.house101@gmail.com>
2022-12-06 14:21:36 +0200

    init-commit

---
+++ app/src/main/java/com/example/intertn/model/SymptomManifest.java
@@ -0,0 +1,19 @@
+package com.example.intertn.model;
+
+public class SymptomManifest {
+    private int Code;
+    private int Probability;
+
+    public SymptomManifest(int Code, int Probability)
+    {
+        this.Code = Code;
+        this.Probability = Probability;
+    }
+
+    public int getCode(){
+        return Code;
+    }
+    public int getProbability(){
+        return Code;
+    }
+}
